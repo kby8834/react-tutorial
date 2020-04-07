2176eb62324c21468d4db8abd4a20312361bd740
kby8834 <62146077+kby8834@users.noreply.github.com>
2020-04-07 17:55:47 +0900

    Update App_Array.js

--- src/App_Array.js
+++ src/App_Array.js
@@ -6,13 +6,15 @@ class App_Array extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      persons: []
+      persons: [],
+      keyword: ''
     };
 
     // 아래 함수에서 this 키워드를 사용하기 위해 바인드 처리를 해준다.
     this.handleCreate = this.handleCreate.bind(this);
     this.handleUpdate = this.handleUpdate.bind(this);
     this.handleRemove = this.handleRemove.bind(this);
+    this.handleKeywordChange = this.handleKeywordChange.bind(this);
   }
 
   handleCreate(e) {
@@ -49,14 +51,15 @@ class App_Array extends Component {
     this.setState({persons: persons.filter(person => person.id !== id)});
   }
 
+  handleKeywordChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
   render() {
     const style = {padding: '5rem'};
-    const tableStyle = {width: '300px', border: '1px solid #000'};
-    const tdStyle = {border: '1px solid #000'};
-
-    const row = this.state.persons.map(person => 
-      <Person key={person.id} handleUpdate={this.handleUpdate} handleRemove={this.handleRemove} person={person}></Person>
-    );
+    const filteredList = this.state.persons.filter(person => person.name.indexOf(this.state.keyword) !== -1);
 
     return (
       <div style={style}>
@@ -67,9 +70,33 @@ class App_Array extends Component {
           <button type="submit">등록</button>
         </form>
 
+        <hr></hr>
+        <input name="keyword" value={this.state.keyword} onChange={this.handleKeywordChange} placeholder="Search Keyword"></input>
+        <PersonList persons={filteredList} handleUpdate={this.handleUpdate} handleRemove={this.handleRemove}></PersonList>
+      </div>
+    );
+  }
+}
+
+class PersonList extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('shouldComponentUpdate');
+    
+    return nextProps.persons !== this.props.persons;
+  }
+
+  render() {
+    const tableStyle = {width: '300px', border: '1px solid #000'};
+    const tdStyle = {border: '1px solid #000'};
+
+    const row = this.props.persons.map(person => 
+      <Person key={person.id} person={person} handleUpdate={this.props.handleUpdate} handleRemove={this.props.handleRemove}></Person>
+    );
+
+    return (
+      <div>
         {row && row.length > 0 && 
           <div>
-            <br></br>
             <table style={tableStyle}>
               <thead>
                 <tr>
@@ -89,7 +116,6 @@ class App_Array extends Component {
 }
 
 class Person extends Component {
-
   handleUpdate(e) {
     const id = this.props.person.id;
     this.props.handleUpdate(id);
